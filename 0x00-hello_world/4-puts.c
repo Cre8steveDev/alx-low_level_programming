3fcae02c37f449c48cc0f7e1ebf1b580e227dd5d
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-15 15:15:09 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * main - Entry point
+ * Description: A function that prints a string programming is like building
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
