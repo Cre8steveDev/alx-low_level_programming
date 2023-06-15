ff7c8ad2cae5c174aac31d5d0e6a0c34f3025e79
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-15 15:33:51 -0700

    Printing a string with the printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - Entry point
+ * Description: A function to print string with printf
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
