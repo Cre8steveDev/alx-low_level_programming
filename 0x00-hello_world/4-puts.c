0c4ef7fad66384c448d022528531c7e0e28ebb5b
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-15 15:17:48 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
  * Description: A function that prints a string programming is like building
  * Return: Always 0 (Success)
  */
+#include <stdio.h>
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle\n");
