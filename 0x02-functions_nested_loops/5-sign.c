328416bbdec827815db35ecc547518efb707fb60
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-20 11:06:20 -0700

    Prints sign and bool equiv

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of negative
+ * @n: The character to compare
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+
+}
