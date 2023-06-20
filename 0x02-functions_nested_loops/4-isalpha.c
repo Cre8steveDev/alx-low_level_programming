030939a02004be555e4da99d30b02bfe18616a81
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-20 10:46:10 -0700

    Checking for alphabets only

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Prints _putchar
+ * @c: The character to compare
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && <= 90 && c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
