9503fe7e8c623c45f275217c58c13f518689fe4c
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-20 15:31:50 -0700

    function de add de two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Adds two integers todether and returns the result
+ * @n: first operand
+ * @o: second operand
+ *
+ * Return: Always int (Success)
+ */
+
+int add(int n, int o)
+{
+		return (n + o);
+}
