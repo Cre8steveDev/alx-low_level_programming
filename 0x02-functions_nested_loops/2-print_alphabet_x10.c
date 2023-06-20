e5813ce676b834e43090624e78e709c7a1a00ff6
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-20 10:27:55 -0700

    Alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,11 @@
 
 void print_alphabet_x10(void)
 {
-	char letr = 'a';
-	int i;
-
-	for (i=0; i<10; i++)
+	for (i = 0; i < 10; i++)
 	{
+		char letr = 'a';
+		int i;
+
 		while (letr < 'z' + 1)
 		{
 			_putchar(letr);
