19dc51a74aeea7c18e7cfe9aaa3153a309e4ffd5
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-20 15:25:09 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,21 +6,21 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int n, m, p;
 
-	for (num = 0; num <= 9; num++)
+	for (n = 0; n <= 9; n++)
 	{
 		_putchar('0');
-		for (mult = 1; mult <= 9; mult++)
+		for (m = 1; m <= 9; m++)
 		{
 			_putchar(',');
 			_putchar(' ');
-			prod = num * mult;
-			if (prod <= 9)
+			prod = n * m;
+			if (p <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+				_putchar((p / 10) + '0');
+			_putchar((p % 10) + '0');
 		}
 		_putchar('\n');
 	}
