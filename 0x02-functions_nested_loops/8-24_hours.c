8e35a9159d80ac2867cde8ee5ab76becd6bc1d74
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-20 14:11:54 -0700

    AAAAA

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,9 @@ for (i = 0; i < 3; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-for (k = 0; k < 6; k++)
+	if (i == 2)
+		break;
+for (k = 1; k < 6; k++)
 {
 for (l = 0; l < 10; l++)
 {
@@ -26,10 +28,6 @@ _putchar(58);
 _putchar('0' + k);
 _putchar('0' + l);
 _putchar('\n');
-if (i == 2 && j == 3 && k == 5 && l == 9)
-{
-break;
-}
 }
 }
 }
