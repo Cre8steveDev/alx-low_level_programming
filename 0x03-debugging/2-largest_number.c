a1670ffada45259862028c28adf92f2356061b8a
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-21 09:45:48 -0700

    larggee

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,23 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b && b > c)
 {
 largest = a;
 }
 else if (b > a && b > c)
+{
+largest = b;
+}
+else if (c > a && a > b)
+{
+largest = c;
+}
+else if (b < a && a > c)
+{
+largest = a;
+}
+else if (a < b && b > c)
 {
 largest = b;
 }
