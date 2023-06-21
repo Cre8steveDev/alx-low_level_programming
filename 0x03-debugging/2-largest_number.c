0abad94b946cac7f47b52c3207c02b86d1196f35
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-21 09:54:27 -0700

    Printing teh largest of 3 numbers with

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,7 +16,15 @@ if (a > b && b > c)
 {
 largest = a;
 }
-else if (b > a && b > c)
+else if (a > c && c > b)
+{
+largest = a;
+}
+else if (b > a && a > c)
+{
+largest = b;
+}
+else if (b > c && c > a)
 {
 largest = b;
 }
@@ -24,15 +32,7 @@ else if (c > a && a > b)
 {
 largest = c;
 }
-else if (b < a && a > c)
-{
-largest = a;
-}
-else if (a < b && b > c)
-{
-largest = b;
-}
-else
+else if (c > b && b > a)
 {
 largest = c;
 }
