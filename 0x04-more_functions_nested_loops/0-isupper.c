32d4cfab3cf9ef1492349f3537e8bd2ccd6096d8
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-22 11:39:54 -0700

    A Function that checks if a Character is uppercase or not. returns 1 if yes and 0 if otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Function checks if char is upper or not
+ * @c: Only parameter in function
+ * Return: Returns 1 if true 0 if false
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && <= 90)
+		return (1);
+	return (0);
+}
