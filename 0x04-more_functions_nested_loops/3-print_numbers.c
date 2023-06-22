187dbba3a8b4e7ecfe5420f57fa988286be97d24
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-22 12:07:20 -0700

    Printing numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function multiplies 2 numbers
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
