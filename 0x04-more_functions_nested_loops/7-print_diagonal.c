fbee4f72b68d632f2c230c4db94f6a76ac0870c5
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-22 14:29:48 -0700

    Printing diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Function multiplies 2 numbers
+ * @n: operand
+ * Return: Returns 1 if true 0 if false
+*/
+
+void print_diagonal(int n)
+{i
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (n > 0)
+		{
+		while (n > 0)
+		{
+			_putchar(32);
+			n--;
+		}
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+}
