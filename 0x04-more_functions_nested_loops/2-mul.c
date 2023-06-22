af155af14ac8e8a0b87947b8ae33a08c4ecb2c44
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-22 11:57:37 -0700

    Custom made isdigit and multiplication function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Function multiplies 2 numbers
+ * @a: first operand
+ * @b: second operand
+ * Return: Returns 1 if true 0 if false
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
