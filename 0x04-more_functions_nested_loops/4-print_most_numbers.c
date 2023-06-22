30226c6aafb15bc67e720c1588cc91de0bbfdd4f
Stephen Omoregie <cr8stevestudios@gmail.com>
2023-06-22 12:19:16 -0700

    Print numbers 0 to 9 excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function prints 0 to 9
+ * Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+		i++
+	}
+	_putchar('\n');
+}
