5c86c214f103f04047084bbb14ea9db1f5c2f466
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-06-23 20:02:07 +0100

    Printing a triangle - printing patters in C

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_triangle - Function multiplies 2 numbers
+ * @size: operand
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+		putchar('\n');
+	else
+	{
+		printf("Helllooo");
+	}
+}
