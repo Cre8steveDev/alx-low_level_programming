62eb86ebac44cc9871cbed63b4a25496e9432bec
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-06-26 16:56:28 +0100

    Resets value passed to it by ref to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - Changes the value of the variable
+ * passed to it by reference
+ * @n: Pointer to integerParameter of the function
+ * Return: Always 0 (Success void function)
+ */
+
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
