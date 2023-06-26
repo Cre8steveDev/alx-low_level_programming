a4ef4a619e65908bfb4a5877a04441f7d92e224f
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-06-26 17:51:17 +0100

    Printing string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,36 +1,27 @@
 /**
  * print_rev - Returns the number of elements in a string
  * @s: String argument to be printed
- * _strlen - Calculate the length of string
- * @s: argument for strlen
- * Return: Always 0 (Success)
+ * Return: Always void (Success)
  */
 
 #include "main.h"
 
-int _strlen(char *s);
-
 void print_rev(char *s)
 {
 	int i;
-	int size = _strlen(s);
-
-	for (i = size - 1; i >= 0; i--)
-	{
-		putchar(s[i]);
-	}
-	putchar('\n');
-}
-
-int _strlen(char *s)
-{
 	int length = 0;
-	int i;
+	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (j = 0; s[i] != '\0'; j++)
 	{
 		length += 1;
 	}
 
-	return (length);
+	for (i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+
+	putchar('\n');
+
 }
