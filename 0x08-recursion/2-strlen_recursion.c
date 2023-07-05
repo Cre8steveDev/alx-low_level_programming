07c814bb0b9aec96e0d8cb1cd0e8172a88dbe252
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-07-05 17:57:15 +0100

    Counting the length of a string recursively

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _print_rev_recursion - Prints a string recursively
+ * @s: string argument of the function
+ * Return: Void always
+ */
+
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+        return 0;
+    return 1 + _strlen_recursion(s + 1);
+}
