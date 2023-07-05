4f6066ace22d8462336a0feae9eda455c1617d7d
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-07-05 17:38:21 +0100

    Implementing the puts function with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,20 @@
 #include "main.h"
+/**
+ * _puts_recursion - Prints a string recursively
+ * @s: string argument of the function
+ * Return: Void always
+ */
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        return;
-    }
-    else
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
