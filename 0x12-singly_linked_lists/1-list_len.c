5f58775e4d6d8cf88454bf9b50435b18433000b9
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-07-27 20:50:31 +0100

    Counting the number of items in a LinkedList

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - A function that returns the lenght of a list
+ * @h - pointer to a list
+ * Return: len
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	if (!h)
+		return (len);
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
