12667e4d34cb4ae44e5df6775c44efb77828be4d
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-08-01 20:11:00 +0100

    Task 1: counting the number of nodes in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,24 @@
 #include "lists.h"
+
 /**
- *
+ * listint_len - Counts the number of nodes in list
+ * @h: Argument pointer to a list
+ * Return: 0 if NULL or len if atleast 1 node
  */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t len = 0;
-    listint_t *ptr = (listint_t *)h;
+	size_t len = 0;
+	listint_t *ptr = (listint_t *)h;
 
-    if (h == NULL)
-        return (0);
+	if (h == NULL)
+		return (0);
 
-    while (ptr)
-    {
-        len++;
-        ptr = ptr->next;
-    }
+	while (ptr)
+	{
+		len++;
+		ptr = ptr->next;
+	}
 
-    return (len);
+	return (len);
 }
