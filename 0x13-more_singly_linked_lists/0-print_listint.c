7d5d98ddf5711d87b912c8229c47d7d4de9a860b
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-08-01 19:50:03 +0100

    Task 0 of More Singly Linked List Project

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints the integer member of a list
+ * @h: Pointer to the head of a node in a list
+ * Return: Returns 0 if no node or num_node if atleast 1 node
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num_node = 0;
+	listint_t *ptr = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (ptr)
+	{
+		printf("%d\n", ptr->n);
+		num_node++;
+		ptr = ptr->next;
+	}
+
+	return (num_node);
+}
