1b716ecc953a8e99c64c9d02d314799984e90890
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-08-01 21:40:48 +0100

    Pops an item from the beginning of a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Removes the first item on a list
+ * @head: Pointer to head of list to be modified
+ * Return: 0 if head is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+    listint_t *head_next;
+    int current_val;
+
+    if (*head == NULL)
+        return (0);
+
+    current_val = (*head)->n;
+    head_next = (*head)->next;
+    free(*head);
+
+    (*head) = head_next;
+
+    return (current_val);
+}
