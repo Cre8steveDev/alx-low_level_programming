6c54712ed2b0b8adf92f42ff8af52b7d7815feb8
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-08-01 20:58:31 +0100

    Free all items in a list and assign the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the allocated memory of list members
+ * and assigns the head to NULL
+ * @head: Pointer to list to be freed
+ * Return: Void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	if (*head == NULL)
+		return;
+
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head = NULL;
+}
