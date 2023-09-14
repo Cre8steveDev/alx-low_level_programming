a8f6b0b912d78b7b4e68c6c9d429c03b14a59465
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-14 14:45:17 +0100

    Doubly Linked List - Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Printing a double linked list
+ * @h: Pointer to the head of the list
+ * Return: Size of the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
