17e337b11f1f381b9a8b5995803a9b4dcb0bfce5
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-14 14:24:29 +0100

    Doubly Linked List - Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,6 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int len = 0;
-	const dlistint_t *ptr = h;
-
-	while (ptr->prev != NULL)
-	{
-		ptr = ptr->prev;
-	}
-	h = ptr;
-
 	while (h != NULL)
 	{
 		len++;
