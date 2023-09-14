b2af4dfa00ffb2a47b471685e8a0f558e89932ed
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-14 21:31:34 +0100

    Doubly Linked List - Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,9 +22,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newNode->prev = NULL;
 	newNode->next = NULL;
 
-	if (*h == NULL || idx == 0)
+	if (*h == NULL && idx == 0)
 	{
-		newNode->next = *h;
 		*h = newNode;
 		return (newNode);
 	}
