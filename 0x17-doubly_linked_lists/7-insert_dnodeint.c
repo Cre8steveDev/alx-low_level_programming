4a3a3b3b862336df182397521251439b0cba39a6
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-14 22:10:08 +0100

    Doubly Linked List - Task 7 v4

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	newNode = malloc(sizeof(dlistint_t));
-	if (newNode == NULL || (idx != 0 && !h))
+	if (newNode == NULL)
 		return (NULL);
 
 	newNode->n = n;
