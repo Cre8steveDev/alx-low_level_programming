1230ebb7c05ddbd4249035ff0dcc24273a23c56c
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-14 20:27:01 +0100

    Doubly Linked List - Task 2

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sum the numbers in a list
+ * @head: Pointer to the head
+ * Return: Sum
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0 ;
+	dlistint *curr = head;
+
+	while
+}
