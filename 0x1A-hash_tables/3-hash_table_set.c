01ebd01bf522f198710f7cc90d25fafeb550dc7b
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-10-08 14:02:10 +0100

    Set item in table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 
-	new_node->key = (char *)key;
+	new_node->key = strdup((char *)key);
 	new_node->value = (char *)value;
 
 	index = key_index((const unsigned char *)key, ht->size);
